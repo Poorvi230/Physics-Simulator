d00c18199539bbb10b8285f79f3e1e9add5116a2
Poorvi230 <poorvigarg12.09@gmail.com>
2026-07-02 14:15:55 +0530

    Improved UI with themes, animations and graph features

--- script.js
+++ script.js
@@ -12,15 +12,27 @@ angleSlider.oninput = function() {
     angleValue.textContent = this.value;
 };
 
-const canvas = document.getElementById('Canvas');
+const heightSlider = document.getElementById('height');
+const heightValue = document.getElementById('heightValue');
+
+heightSlider.oninput = function() {
+    heightValue.textContent = this.value;
+};
+
+const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 const button = document.getElementById('launchBtn');
+const themeSelector = document.getElementById("themeSelect");
+themeSelector.addEventListener("change", () => {
+    document.body.className = "space";
+});
 button.addEventListener("click", launchProjectile);
 
 function launchProjectile() {
     let x= 50;
-    let y= 350;
+    let initialHeight = Number(heightSlider.value);
+    let y= 350 - initialHeight *2;
     let velocity = Number(velocitySlider.value);  //horizontal speed
     let angle = Number(angleSlider.value); // initial angle
 
@@ -28,11 +40,34 @@ function launchProjectile() {
 
     let dx = velocity * Math.cos(angleRad) * 0.2; 
     let dy = -velocity * Math.sin(angleRad) * 0.2;
-    
+
     let gravity= 0.3;
     let animation = setInterval(() => {
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    //Draw X axis
+    ctx.beginPath();
+    ctx.moveTo(50, 350);
+    ctx.lineTo(750, 350);
+    ctx.strokeStyle = 'black';
+    ctx.lineWidth = 2;
+    ctx.stroke();
+
+    //Draw Y axis
+    ctx.beginPath();
+    ctx.moveTo(50, 350);
+    ctx.lineTo(50, 50);
+    ctx.strokeStyle = 'black';
+    ctx.lineWidth = 2;
+    ctx.stroke();
+
+    //Axis labels
+    ctx.font = '20px Arial';
+    ctx.fillStyle = 'black';
+    ctx.fillText('X', 760, 355);
+    ctx.fillText('Y', 40, 40);
+
 dy += gravity;
 
 x += dx;
